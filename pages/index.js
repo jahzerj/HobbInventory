cc7606596124ea8b1d36d71173e8f2ecaaab417f
Jahaziel <jahzerahj@gmail.com>
2025-02-17 19:19:21 +0100

    added an ugly button

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledButton = styled(button)`
+  box-shadow: 6px 4px 6px 0px #3dc21b;
+  background-color: #44c767;
+  border-radius: 21px;
+  border: 2px solid #18ab29;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 17px;
+  font-weight: bold;
+  padding: 32px 76px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #2f6627;
+  &: hover {
+    background-color: rgb(37, 17, 216);
+  }
+`;
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <StyledButton>Keycap Inventory</StyledButton>
     </div>
   );
 }
