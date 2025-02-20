c89ca07a3c01ce5c4cd5288c3eaf227a30759daf
Jahaziel <jahzerahj@gmail.com>
2025-02-20 18:14:06 +0100

    finished inventory card, keycaps page

---
+++ components/InventoryCard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledCard = styled.div`
+  background-color: lightgreen;
+  width: 60%;
+  border-radius: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 15px;
+  margin: 10px;
+  height: 250px;
+  justify-content: space-between;
+  box-shadow: 10px 5px 5px darkgreen;
+`;
+
+const StyledDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2px;
+  margin-bottom: 5px;
+
+  p {
+    margin: 0; /* ✅ Remove extra spacing between lines */
+  }
+`;
+
+export default function InventoryCard({ data }) {
+  return (
+    <>
+      {data.map((keycap) => (
+        <StyledCard key={keycap._id}>
+          <h3>CYL {keycap.name}</h3>
+          <StyledDetails>
+            <p>Manufacturer: {keycap.keycapstype}</p>
+            <p>Profile: {keycap.profile}</p>
+            <p>Designer: {keycap.designer}</p>
+          </StyledDetails>
+        </StyledCard>
+      ))}
+    </>
+  );
+}
---
+++ pages/inventories/keycaps.js
@@ -0,0 +1,76 @@
+import InventoryCard from "@/components/InventoryCard";
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default function Keycaps() {
+  return (
+    <>
+      <Link href="/">Back to Hub</Link>
+      <StyledContainer>
+        <h1>Keycap Inventory</h1>
+        {data.length > 0 ? (
+          <InventoryCard data={data} />
+        ) : (
+          <>
+            <p> You have no keycaps in your inventory!</p>
+            <p> Click the ➕ button to add a keycap set</p>
+          </>
+        )}
+      </StyledContainer>
+    </>
+  );
+}
+
+const data = [
+  {
+    _id: {
+      $oid: "67af15ccba07c6a2595b086d",
+    },
+    name: 1520,
+    keycapstype: "GMK",
+    designer: "hark",
+    profile: "1-1-2-3-4-4",
+    link: "https://geekhack.org/index.php?topic=122578.0",
+    render_pics: [
+      "https://novelkeys.com/cdn/shop/files/CYL_1520_Quetzal_Wide_1512x.png",
+    ],
+    kits: {
+      $oid: "67b72605f6356be9084cae6b",
+    },
+  },
+  {
+    _id: {
+      $oid: "67af15ccba07c6a2595b0898",
+    },
+    name: "Botanical R2",
+    keycapstype: "GMK",
+    designer: "Hazzy",
+    profile: "1-1-2-3-4-4",
+    render_pics: [
+      "https://cdn.shopify.com/s/files/1/0054/0878/4458/products/GMK_Botanical_2_OMNI_Bauer_X_001V2_dist_800x.jpg",
+    ],
+    kits: {
+      $oid: "67b72605f6356be9084cae96",
+    },
+  },
+  {
+    _id: {
+      $oid: "67af15ccba07c6a2595b08d3",
+    },
+    name: "Foundation",
+    keycapstype: "GMK",
+    designer: "HungHingDaiLo",
+    profile: "1-1-2-3-4-4",
+    link: "https://geekhack.org/index.php?topic=113538.0",
+    render_pics: ["https://i.imgur.com/FkJ57Po.png"],
+    kits: {
+      $oid: "67b72605f6356be9084caed1",
+    },
+  },
+];
