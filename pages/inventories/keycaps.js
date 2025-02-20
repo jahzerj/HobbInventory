feae58d11fcbaa553810107ca97d7ab6583466df
Jahaziel <jahzerahj@gmail.com>
2025-02-20 16:03:21 +0100

    db connection tested thoroughly

--- pages/inventories/keycaps.js
+++ pages/inventories/keycaps.js
@@ -1,4 +1,3 @@
-import Dropdown from "@/components/Dropdown.js";
 import Link from "next/link";
 
 export default function Keycaps() {
@@ -8,7 +7,6 @@ export default function Keycaps() {
       <h1>Keycap Inventory</h1>
       <p> You have no keycaps in your inventory!</p>
       <p> Click the ➕ button to add a keycap set</p>
-      <Dropdown />
     </>
   );
 }
