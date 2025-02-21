2926a1e89bcafa532f3aac3957160018a60d5719
Jahaziel <jahzerahj@gmail.com>
2025-02-21 14:44:42 +0100

    working on modal

--- pages/inventories/keycaps.js
+++ pages/inventories/keycaps.js
@@ -1,5 +1,20 @@
 import InventoryCard from "@/components/InventoryCard";
 import Link from "next/link";
+import AddButtton from "@/components/AddButtton";
+import Modal from "@/components/Modal";
+import { useState } from "react";
+
+const BUTTON_WRAPPER_STYLES = {
+  position: "relative",
+  zIndex: 1,
+};
+
+const OTHER_CONTENT_STYLES = {
+  position: "relative",
+  zIndex: 2,
+  backgroundColor: "orange",
+  padding: "10px",
+};
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
@@ -9,9 +24,19 @@ const StyledContainer = styled.div`
 `;
 
 export default function Keycaps() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <Link href="/">Back to Hub</Link>
+      <h1>Keycap Inventory</h1>
+      <div style={BUTTON_WRAPPER_STYLES}>
+        <button onClick={() => setIsOpen(true)}> Open Modal </button>
+        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+          Fancy Modal
+        </Modal>
+      </div>
+      <div style={OTHER_CONTENT_STYLES}>Other Content</div>
+      <AddButtton />
       <StyledContainer>
         <h1>Keycap Inventory</h1>
         {data?.length ? (
