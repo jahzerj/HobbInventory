0bef77b9ab18024e5d2969aa7eef5d053206acf9
Jahaziel <jahzerahj@gmail.com>
2025-02-21 19:00:08 +0100

    working dropdowns

--- pages/inventories/keycaps.js
+++ pages/inventories/keycaps.js
@@ -3,22 +3,15 @@ import AddButtton from "@/components/AddButtton";
 import Modal from "@/components/Modal";
 import { useState } from "react";
 
-const BUTTON_WRAPPER_STYLES = {
-  position: "relative",
-  zIndex: 1,
-};
-
 export default function Keycaps() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <Link href="/">Back to Hub</Link>
       <h1>Keycap Inventory</h1>
-      <div style={BUTTON_WRAPPER_STYLES}>
-        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-          THIS TEXT APPEARS INSIDE MODAL
-        </Modal>
-      </div>
+      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+        THIS TEXT APPEARS INSIDE MODAL
+      </Modal>
 
       <AddButtton onOpenModal={() => setIsOpen(true)} />
     </>
