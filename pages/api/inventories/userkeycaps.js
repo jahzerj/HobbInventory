8fae74940cbfb3499312ba4c3f8b68b26cb341e3
Jahaziel <jahzerahj@gmail.com>
2025-03-25 20:39:40 +0100

    finally fixed the duplication bug. removed extra put method from userkeycaps api.

--- pages/api/inventories/userkeycaps.js
+++ pages/api/inventories/userkeycaps.js
@@ -12,53 +12,6 @@ export default async function handler(req, res) {
       return res.status(200).json(userKeycaps);
     }
 
-    if (req.method === "POST") {
-      const {
-        keycapDefinitionId,
-        selectedKits,
-        selectedColors = [],
-        notes = [],
-      } = req.body;
-
-      // If adding from keycap definition
-      if (keycapDefinitionId) {
-        const keycapDef = await Keycapdefinition.findById(keycapDefinitionId);
-        if (!keycapDef) {
-          return res
-            .status(404)
-            .json({ message: "Keycap definition not found" });
-        }
-
-        const userKeycap = await UserKeycap.create({
-          userId,
-          name: keycapDef.name,
-          manufacturer: keycapDef.manufacturer,
-          profile: keycapDef.profile,
-          material: keycapDef.material,
-          profileHeight: keycapDef.profileHeight,
-          designer: keycapDef.designer,
-          geekhacklink: keycapDef.geekhacklink,
-          render: keycapDef.render,
-          kits: keycapDef.kits,
-          selectedKits,
-          selectedColors,
-          notes,
-          keycapDefinitionId,
-        });
-
-        return res.status(201).json(userKeycap);
-      }
-
-      // If manual entry
-      const userKeycap = await UserKeycap.create({
-        userId,
-        ...req.body,
-        isManualEntry: true,
-      });
-
-      return res.status(201).json(userKeycap);
-    }
-
     if (req.method === "POST" || req.method === "PUT") {
       const { keycapDefinitionId, selectedKits, selectedColors, notes } =
         req.body;
